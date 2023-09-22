53e07e4967452edce4ed75bf1106d7632ddce171
SR1646 <patelsandip445566@gmail.com>
2023-09-22 11:03:04 +0530

    Extra code added

--- src/main/java/sr/com/sr/va/Main.java
+++ src/main/java/sr/com/sr/va/Main.java
@@ -1,9 +1,14 @@
 package sr.com.sr.va;
 
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
-	    FileRenamer fileRenamer = new FileRenamer();
+//	    FileRenamer fileRenamer = new FileRenamer();
 //        fileRenamer.renameFileByRemovingSpecialChar("E:\\AS\\entertainment\\movies\\hollywood\\watched movies\\Unrated");
+//        new MarkCounter().countMark();
+            new Base64Helper().convert();
+
     }
 }
